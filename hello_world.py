1a6410e1ccea72488e9e0f2268d77552dddf92b3
Nate Sirivallop <nsirival1@gmail.com>
2025-07-02 18:57:04 -0700

    s

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,20 @@
-def mean(input):
-  sum = 0
-  for entry in input:
-    sum += entry
-  return sum / len(input)
+import math
 
-my_list = [0, 1, 2, 3, 4, 5]
-print(mean(my_list))
+def median(input):
 
+  input.sort()
+
+  listlen = len(my_list)
+  midlist = listlen//2
+  if listlen == 0:
+    return ("list is empty")
+
+  if listlen % 2 == 0:
+    return((input[midlist-1] + input[midlist])/2)
+
+  else:
+    return(input[midlist])
+
+
+my_list = [0, 3, 5, 7, 10, 11, 32, 87, 100, 999]
+print(median(my_list))
