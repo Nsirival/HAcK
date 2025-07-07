2fb20bc817bb4b19b455240faf503fdc4476741a
Nate Sirivallop <nsirival1@gmail.com>
2025-07-06 18:43:42 -0700

    added median

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,21 @@
-print ("hello world")
+import math
+
+def median(input):
+
+  input.sort()
+
+  listlen = len(my_list)
+  midlist = listlen//2
+  if listlen == 0:
+    return ("list is empty")
+
+  if listlen % 2 == 0:
+    return((input[midlist-1] + input[midlist])/2)
+
+  else:
+    return(input[midlist])
+
+
+my_list = [0, 3, 5, 7, 10, 11, 32, 87, 100, 999]
+print(median(my_list))
+
